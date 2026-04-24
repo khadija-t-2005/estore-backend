60d7448e569005d3bdfbad5a9e3cc48d87f37510
Marwa Benhaddi‬‎ <151185746+marwa-benhaddi@users.noreply.github.com>
2026-04-24 08:25:59 -0700

    Update OrderProcessingService.java

--- src/main/java/com/estore/estorebackend/billing/service/OrderProcessingService.java
+++ src/main/java/com/estore/estorebackend/billing/service/OrderProcessingService.java
@@ -1,4 +1,34 @@
-package com.estore.estorebackend.billing.service;
+package com.estore.estorebackend.billing.service.impl;
 
-public interface OrderProcessingService {
+import com.estore.estorebackend.billing.dto.OrderDTO;
+import com.estore.estorebackend.billing.dto.OrderStatusDTO;
+import com.estore.estorebackend.billing.entity.Order;
+import com.estore.estorebackend.billing.repository.OrderRepository;
+import com.estore.estorebackend.billing.service.OrderProcessingService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@RequiredArgsConstructor
+public class OrderProcessingServiceImpl implements OrderProcessingService {
+
+    private final OrderRepository orderRepository;
+
+    @Override
+    @Transactional
+    public OrderDTO placeOrder(Long userId) {
+        // La logique pour transformer le Cart en Order viendra ici plus tard
+        return null;
+    }
+
+    @Override
+    @Transactional
+    public void updateStatus(OrderStatusDTO statusDTO) {
+        Order order = orderRepository.findById(statusDTO.getOrderId())
+                .orElseThrow(() -> new RuntimeException("Commande introuvable avec l'ID : " + statusDTO.getOrderId()));
+
+        order.setStatus(statusDTO.getStatus());
+        orderRepository.save(order);
+    }
 }
