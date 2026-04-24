2054bae1f427d1ce743a51010f17c1d3912e02b6
Marwa Benhaddi‬‎ <151185746+marwa-benhaddi@users.noreply.github.com>
2026-04-24 08:19:21 -0700

    Update OrderDTO.java

--- src/main/java/com/estore/estorebackend/billing/dto/OrderDTO.java
+++ src/main/java/com/estore/estorebackend/billing/dto/OrderDTO.java
@@ -1,4 +1,13 @@
 package com.estore.estorebackend.billing.dto;
 
+import lombok.Data;
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
+@Data
 public class OrderDTO {
+    private Long id;
+    private LocalDateTime orderDate;
+    private BigDecimal totalAmount;
+    private String status;
 }
