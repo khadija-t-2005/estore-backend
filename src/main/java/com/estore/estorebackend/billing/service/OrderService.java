2741a9b41436dcb72b9a0dd5050c1f6518faf6a3
Marwa Benhaddi‬‎ <151185746+marwa-benhaddi@users.noreply.github.com>
2026-04-24 08:28:44 -0700

    Update OrderService.java

--- src/main/java/com/estore/estorebackend/billing/service/OrderService.java
+++ src/main/java/com/estore/estorebackend/billing/service/OrderService.java
@@ -1,4 +1,10 @@
 package com.estore.estorebackend.billing.service;
 
+import com.estore.estorebackend.billing.dto.OrderDTO;
+import com.estore.estorebackend.billing.dto.OrderDetailDTO;
+import java.util.List;
+
 public interface OrderService {
+    List<OrderDTO> getOrdersByUserId(Long userId);
+    OrderDetailDTO getOrderById(Long orderId);
 }
