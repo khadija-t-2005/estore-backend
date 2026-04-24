34c0f00167135b54a3e3f8fe23187ca79e713272
Marwa Benhaddi‬‎ <151185746+marwa-benhaddi@users.noreply.github.com>
2026-04-24 08:25:24 -0700

    Update OrderRepository.java

--- src/main/java/com/estore/estorebackend/billing/repository/OrderRepository.java
+++ src/main/java/com/estore/estorebackend/billing/repository/OrderRepository.java
@@ -1,4 +1,12 @@
 package com.estore.estorebackend.billing.repository;
 
-public interface OrderRepository {
+import com.estore.estorebackend.billing.entity.Order;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface OrderRepository extends JpaRepository<Order, Long> {
+    List<Order> findByUserIdOrderByOrderDateDesc(Long userId);
 }
