28210984351256fdffd63fa02dc128566a41fa17
Marwa Benhaddi‬‎ <151185746+marwa-benhaddi@users.noreply.github.com>
2026-04-24 08:20:21 -0700

    Update OrderItemDTO.java

--- src/main/java/com/estore/estorebackend/billing/dto/OrderItemDTO.java
+++ src/main/java/com/estore/estorebackend/billing/dto/OrderItemDTO.java
@@ -1,4 +1,13 @@
 package com.estore.estorebackend.billing.dto;
 
+import lombok.Data;
+import java.math.BigDecimal;
+
+@Data
 public class OrderItemDTO {
+    private Long productId;
+    private String productName;
+    private Integer quantity;
+    private BigDecimal unitPrice;
+    private BigDecimal subTotal;
 }
