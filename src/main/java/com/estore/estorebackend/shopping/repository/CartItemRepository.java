6138bf5daec21af0106c4111be43c1cb89e2c396
amal940 <ennairamal@gmail.com>
2026-04-26 14:29:18 +0200

    Update CartItemRepository.java

--- src/main/java/com/estore/estorebackend/shopping/repository/CartItemRepository.java
+++ src/main/java/com/estore/estorebackend/shopping/repository/CartItemRepository.java
@@ -1,4 +1,12 @@
-package com.estore.estorebackend.shopping.repository;
+package com.estore.estoreProject.shopping.repository;
 
-public interface CartItemRepository {
+import com.estore.estoreProject.shopping.entity.CartItem;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface CartItemRepository extends JpaRepository<CartItem, Long> {
+    List<CartItem> findByCartId(Long cartId);
+    Optional<CartItem> findByCartIdAndProductId(Long cartId, Long productId);
 }
