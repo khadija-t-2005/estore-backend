2adda9f08838e0db24986d3b016debf41f1d5e49
amal940 <ennairamal@gmail.com>
2026-04-26 14:27:28 +0200

    Update CartItem.java

--- src/main/java/com/estore/estorebackend/shopping/entity/CartItem.java
+++ src/main/java/com/estore/estorebackend/shopping/entity/CartItem.java
@@ -1,4 +1,82 @@
-package com.estore.estorebackend.shopping.entity;
+package com.estore.estoreProject.shopping.entity;
 
+import com.estore.estoreProject.catalog.entity.Product;
+import jakarta.persistence.*;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotNull;
+
+@Entity
+@Table(name = "cart_items")
 public class CartItem {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @NotNull(message = "La quantité est obligatoire")
+    @Min(value = 1, message = "La quantité doit être au moins 1")
+    private Integer quantity;
+
+    @NotNull(message = "Le prix unitaire est obligatoire")
+    @Min(value = 0, message = "Le prix unitaire ne peut pas être négatif")
+    private Double unitPrice;
+
+    @ManyToOne
+    @JoinColumn(name = "cart_id")
+    private Cart cart;
+
+    @ManyToOne
+    @JoinColumn(name = "product_id")
+    private Product product;
+
+    public CartItem() {
+    }
+
+    public CartItem(Long id, Integer quantity, Double unitPrice, Cart cart, Product product) {
+        this.id = id;
+        this.quantity = quantity;
+        this.unitPrice = unitPrice;
+        this.cart = cart;
+        this.product = product;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Integer getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(Integer quantity) {
+        this.quantity = quantity;
+    }
+
+    public Double getUnitPrice() {
+        return unitPrice;
+    }
+
+    public void setUnitPrice(Double unitPrice) {
+        this.unitPrice = unitPrice;
+    }
+
+    public Cart getCart() {
+        return cart;
+    }
+
+    public void setCart(Cart cart) {
+        this.cart = cart;
+    }
+
+    public Product getProduct() {
+        return product;
+    }
+
+    public void setProduct(Product product) {
+        this.product = product;
+    }
 }
