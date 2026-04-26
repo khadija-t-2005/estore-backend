08c5f17b5086bfca9f38d429a2101a04f58a48c3
amal940 <ennairamal@gmail.com>
2026-04-26 14:26:27 +0200

    Update Cart.java

--- src/main/java/com/estore/estorebackend/shopping/entity/Cart.java
+++ src/main/java/com/estore/estorebackend/shopping/entity/Cart.java
@@ -1,4 +1,56 @@
-package com.estore.estorebackend.shopping.entity;
+package com.estore.estoreProject.shopping.entity;
 
+import com.estore.estoreProject.customer.entity.User;
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotNull;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "carts")
 public class Cart {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @NotNull(message = "La date de création est obligatoire")
+    private LocalDateTime createdAt;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id")
+    private User user;
+
+    public Cart() {
+    }
+
+    public Cart(Long id, LocalDateTime createdAt, User user) {
+        this.id = id;
+        this.createdAt = createdAt;
+        this.user = user;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public LocalDateTime getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(LocalDateTime createdAt) {
+        this.createdAt = createdAt;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
 }
