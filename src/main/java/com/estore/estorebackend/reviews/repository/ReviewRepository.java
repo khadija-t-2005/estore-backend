daf562652bd72bd1fc5e687c769088ef5db202fd
hiba-tech-20 <hiba.hlility2005@gmail.com>
2026-04-26 01:09:54 +0100

    Extend ReviewRepository to use MongoDB features

--- src/main/java/com/estore/estorebackend/reviews/repository/ReviewRepository.java
+++ src/main/java/com/estore/estorebackend/reviews/repository/ReviewRepository.java
@@ -1,4 +1,15 @@
 package com.estore.estorebackend.reviews.repository;
 
-public interface ReviewRepository {
+import com.estore.estorebackend.reviews.document.Review;
+import org.springframework.data.mongodb.repository.MongoRepository;
+
+import java.util.List;
+
+public interface ReviewRepository extends MongoRepository<Review, String> {
+
+    List<Review> findByProductId(Long productId);
+
+    List<Review> findByUserId(Long userId);
+
+    boolean existsByUserIdAndProductId(Long userId, Long productId);
 }
