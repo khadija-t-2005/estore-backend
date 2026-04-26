d720571875ddc24a1671c42024d6b0feafe0fc89
hiba-tech-20 <hiba.hlility2005@gmail.com>
2026-04-26 01:08:06 +0100

    Add fields and methods to ReviewDTO class

--- src/main/java/com/estore/estorebackend/reviews/dto/ReviewDTO.java
+++ src/main/java/com/estore/estorebackend/reviews/dto/ReviewDTO.java
@@ -1,4 +1,41 @@
 package com.estore.estorebackend.reviews.dto;
 
 public class ReviewDTO {
+
+    private Long userId;
+    private Long productId;
+    private int rating;
+    private String comment;
+
+    public Long getUserId() {
+        return userId;
+    }
+
+    public Long getProductId() {
+        return productId;
+    }
+
+    public int getRating() {
+        return rating;
+    }
+
+    public String getComment() {
+        return comment;
+    }
+
+    public void setUserId(Long userId) {
+        this.userId = userId;
+    }
+
+    public void setProductId(Long productId) {
+        this.productId = productId;
+    }
+
+    public void setRating(int rating) {
+        this.rating = rating;
+    }
+
+    public void setComment(String comment) {
+        this.comment = comment;
+    }
 }
