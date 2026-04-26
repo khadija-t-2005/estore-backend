f6c9958b8d4ab9000901604bf323f6a728aeb1fb
amal940 <ennairamal@gmail.com>
2026-04-26 14:29:54 +0200

    Update CartRepository.java

--- src/main/java/com/estore/estorebackend/shopping/repository/CartRepository.java
+++ src/main/java/com/estore/estorebackend/shopping/repository/CartRepository.java
@@ -1,4 +1,12 @@
-package com.estore.estorebackend.shopping.repository;
+package com.estore.estoreProject.shopping.repository;
 
-public interface CartRepository {
+import com.estore.estoreProject.shopping.entity.Cart;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface CartRepository extends JpaRepository<Cart, Long> {
+    List<Cart> findByUserId(Long userId);
+    Optional<Cart> findTopByUserIdOrderByCreatedAtDesc(Long userId);
 }
