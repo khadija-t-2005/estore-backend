19f2e41df80798a381d916a08654e4b1fea27a68
hiba-tech-20 <hiba.hlility2005@gmail.com>
2026-04-26 01:10:20 +0100

    Add methods for review management in ReviewService

--- src/main/java/com/estore/estorebackend/reviews/service/ReviewService.java
+++ src/main/java/com/estore/estorebackend/reviews/service/ReviewService.java
@@ -1,4 +1,17 @@
 package com.estore.estorebackend.reviews.service;
 
+import com.estore.estorebackend.reviews.dto.ReviewDTO;
+import com.estore.estorebackend.reviews.dto.ReviewResponseDTO;
+
+import java.util.List;
+
 public interface ReviewService {
+
+    ReviewResponseDTO createReview(ReviewDTO reviewDTO);
+
+    List<ReviewResponseDTO> getReviewsByProduct(Long productId);
+
+    List<ReviewResponseDTO> getReviewsByUser(Long userId);
+
+    void deleteReview(String reviewId);
 }
