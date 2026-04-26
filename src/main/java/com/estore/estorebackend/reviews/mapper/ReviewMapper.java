0a8e6a251d10fce256b711c6f8902c8c4b1d9543
hiba-tech-20 <hiba.hlility2005@gmail.com>
2026-04-26 01:09:25 +0100

    Implement ReviewMapper for converting DTOs and entities

--- src/main/java/com/estore/estorebackend/reviews/mapper/ReviewMapper.java
+++ src/main/java/com/estore/estorebackend/reviews/mapper/ReviewMapper.java
@@ -1,4 +1,29 @@
 package com.estore.estorebackend.reviews.mapper;
 
+import com.estore.estorebackend.reviews.document.Review;
+import com.estore.estorebackend.reviews.dto.ReviewDTO;
+import com.estore.estorebackend.reviews.dto.ReviewResponseDTO;
+import org.springframework.stereotype.Component;
+
+@Component
 public class ReviewMapper {
+
+    public Review toEntity(ReviewDTO dto) {
+        Review review = new Review();
+        review.setUserId(dto.getUserId());
+        review.setProductId(dto.getProductId());
+        review.setRating(dto.getRating());
+        review.setComment(dto.getComment());
+        return review;
+    }
+
+    public ReviewResponseDTO toResponseDTO(Review review) {
+        ReviewResponseDTO dto = new ReviewResponseDTO();
+        dto.setId(review.getId());
+        dto.setUserId(review.getUserId());
+        dto.setProductId(review.getProductId());
+        dto.setRating(review.getRating());
+        dto.setComment(review.getComment());
+        return dto;
+    }
 }
