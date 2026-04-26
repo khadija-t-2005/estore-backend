795a25c9cb8afd9033e9abaf9423806f551825ae
hiba-tech-20 <hiba.hlility2005@gmail.com>
2026-04-26 01:07:38 +0100

    Add Review class for product reviews

--- src/main/java/com/estore/estorebackend/reviews/document/Review.java
+++ src/main/java/com/estore/estorebackend/reviews/document/Review.java
@@ -1,4 +1,58 @@
 package com.estore.estorebackend.reviews.document;
 
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+@Document(collection = "reviews")
 public class Review {
+
+    @Id
+    private String id;
+
+    private Long userId;
+    private Long productId;
+    private int rating;
+    private String comment;
+
+    public Review() {}
+
+    public String getId() {
+        return id;
+    }
+
+    public Long getUserId() {
+        return userId;
+    }
+
+    public Long getProductId() {
+        return productId;
+    }
+
+    public int getRating() {
+        return rating;
+    }
+
+    public String getComment() {
+        return comment;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public void setUserId(Long userId) {
+        this.userId = userId;
+    }
+
+    public void setProductId(Long productId) {
+        this.productId = productId;
+    }
+
+    public void setRating(int rating) {
+        this.rating = rating;
+    }
+
+    public void setComment(String comment) {
+        this.comment = comment;
+    }
 }
